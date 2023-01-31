a3e7125e55d230d858d3e7f3ec4a712a7b978a13
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2023-01-31 14:41:14 -0500

    Added /blogs route.

---
+++ routes/blogs.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({
+    success: true,
+    route: "blogs",
+    message: "Welcome to the blogs page."
+  });
+});
+
+module.exports = router;
